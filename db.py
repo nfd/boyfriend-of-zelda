b2e2de28e06b4e5d63999163961f36392fb140a1
Nicholas FitzRoy-Dale <nicholas@fitzroydale.org>
2021-03-25 17:18:33 +0000

    db: use write-ahead log mode

--- db.py
+++ db.py
@@ -32,6 +32,7 @@ def get_db():
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
+        g.db.execute("PRAGMA journal_mode=wal")
 
     return g.db
 
